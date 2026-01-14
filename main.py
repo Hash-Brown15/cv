023ec9ee748b880127f198929527af71fd0114a5
Hash-Brown15 <JackHeppell1@gmail.com>
2026-01-14 21:02:17 +0000

    Add files via upload

--- main.py
+++ main.py
@@ -1,8 +1,10 @@
 import cv2
 import os
+import numpy as np
 print(cv2.__version__)
 img = cv2.imread("image.jpg",cv2.IMREAD_COLOR)
 B,G,R = cv2.split(img)
+zero = np.zeros(img.shape[:2],dtype="uint8")
 cv2.imshow("wallpaper",img)
 
 save_directry = "/Users/jinheppell/Desktop/Coding/open cv/images"
@@ -10,11 +12,9 @@ save_directry = "/Users/jinheppell/Desktop/Coding/open cv/images"
 os.chdir(save_directry)
 cv2.imwrite("landscape.jpg",img)
 
-cv2.imshow("Blue",B)
-cv2.waitKey(0)
-cv2.imshow("Green",G)
-cv2.waitKey(0)
-cv2.imshow("Red",R)
+cv2.imshow("Blue",cv2.merge([B,zero,zero]))
+cv2.imshow("Green",cv2.merge([zero,G,zero]))
+cv2.imshow("Red",cv2.merge([zero,zero,R]))
 cv2.waitKey(0)
 
 cv2.destroyAllWindows()
